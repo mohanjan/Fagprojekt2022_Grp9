00e4c327f28388c849766670634d74f50cf18380
mohanjan <jwbartling.johan@gmail.com>
2022-02-21 15:12:57 +0100

    Update ISA.java

--- Software_files/bit-ifyer/src/ISA.java
+++ Software_files/bit-ifyer/src/ISA.java
@@ -1,8 +1,61 @@
+
+
 public class ISA{
 
     //method to read a string and shit out numbers
-    public int {
+    public int find_name(String instruction){
+        int val=0;
+        if(instruction.contains("ADD"||"add"||"Add"){
+            val=0b0001;
+            
+        }
 
+        else if(instruction.contains("SUB"||"sub"||"Sub")){
+            
+        }
+
+        else if(instruction.contains("MULT"||"mult"||"Mult")){
+            
+        }
+
+        else if(){
+            
+        }
+
+        else if(){
+            
+        }
+
+        else if(){
+            
+        }
+
+        else if(){
+            
+        }
+
+        else if(){
+            
+        }
+
+        else if(){
+            
+        }
+
+        else if(){
+            
+        }
+
+        else if(){
+            
+        }
+
+
+
+
+
+
+        return val;
     }
 
 }
