837857b8fdb871a9ea3abcd012556b8ba623c4a6
Karl Krause <66793169+McHerman@users.noreply.github.com>
2022-04-23 15:20:31 +0200

    Added new top module

--- src/main/scala/DSP.scala
+++ src/main/scala/DSP.scala
@@ -2,16 +2,6 @@ import chisel3._
 import chisel3.experimental.Analog
 import chisel3.util._
 
-class MemPort extends Bundle{
-  val Address = Output(UInt(18.W))
-  val WriteData = Output(UInt(18.W))
-  val Enable = Output(Bool())
-  val WriteEn = Output(Bool())
-
-  val ReadData = Input(UInt(18.W))
-  val Completed = Input(Bool())
-}
-
 class DSP(maxCount: Int) extends Module {
   val io = IO(new Bundle {
     val In = Input(UInt(16.W))
@@ -25,37 +15,14 @@ class DSP(maxCount: Int) extends Module {
     val Drive = Output(Bool())
   })
 
-  // Single Core
+  val SubDSP = Module(new SubDSP())
 
-  val Core = Module(new Core())
-  val FirEngine = Module(new FirEngine())
-  val DataMemory = Module(new DataMemory())
-
-  // IO
-
-  io.Out := Core.io.WaveOut
-  FirEngine.io.WaveIn := io.In
-
-  // Interconnections
-
-  Core.io.WaveIn := 0.U
-  Core.io.Stall := false.B
-  Core.io.ProgramLength := 0.U
-
-  Core.io.MemPort <> DataMemory.io.MemPort
-
-  FirEngine.io.Registers <> DataMemory.io.Registers
-
-  FirEngine.io.MemPort <> DataMemory.io.FIRMemPort
-  FirEngine.io.WaveIn := 0.U
-
-  SPI <> DataMemory.SPI
-
-  // TODO Add new top module so this one is only a single Core
+  SubDSP.io <> io
+  SubDSP.SPI <> SPI
 
 }
 // generate Verilog
 object DSP extends App {
-  (new chisel3.stage.ChiselStage).emitVerilog(new DSP(200000000))
+  (new chisel3.stage.ChiselStage).emitVerilog(new DSP(100000000))
 }
 
---
+++ src/main/scala/SubDSP.scala
@@ -0,0 +1,54 @@
+import chisel3._
+import chisel3.experimental.Analog
+import chisel3.util._
+
+class MemPort extends Bundle{
+  val Address = Output(UInt(18.W))
+  val WriteData = Output(UInt(18.W))
+  val Enable = Output(Bool())
+  val WriteEn = Output(Bool())
+
+  val ReadData = Input(UInt(18.W))
+  val Completed = Input(Bool())
+}
+
+class SubDSP extends Module {
+  val io = IO(new Bundle {
+    val In = Input(UInt(16.W))
+    val Out = Output(UInt(16.W))
+  })
+  val SPI = IO(new Bundle{
+    val SCLK = Output(Bool())
+    val CE = Output(Bool())
+    val SO = Input(Vec(4,Bool()))
+    val SI = Output(Vec(4,Bool()))
+    val Drive = Output(Bool())
+  })
+
+  // Single Core
+
+  val Core = Module(new Core())
+  val FirEngine = Module(new FirEngine())
+  val DataMemory = Module(new DataMemory())
+
+  // IO
+
+  io.Out := Core.io.WaveOut
+  FirEngine.io.WaveIn := io.In
+
+  // Interconnections
+
+  Core.io.WaveIn := 0.U
+  Core.io.Stall := false.B
+  Core.io.ProgramLength := 0.U
+
+  Core.io.MemPort <> DataMemory.io.MemPort
+
+  FirEngine.io.Registers <> DataMemory.io.Registers
+
+  FirEngine.io.MemPort <> DataMemory.io.FIRMemPort
+  FirEngine.io.WaveIn := 0.U
+
+  SPI <> DataMemory.SPI
+
+}
