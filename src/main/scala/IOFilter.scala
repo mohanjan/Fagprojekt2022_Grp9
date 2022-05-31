c12425854b82a90d9625885dece9429e8b73b654
johtok <johannes@tokes.dk>
2022-06-01 00:51:37 +0200

    WIP IOFILTER with failing test due to weird $anonfun$new$3 error

--- src/main/scala/IOFilter.scala
+++ src/main/scala/IOFilter.scala
@@ -3,33 +3,31 @@ import chisel3.util._
 
 class IOFilter(filterLength: Int) extends Module {
   val io = IO(new Bundle {
-    val WaveIn = Input(UInt(18.W))
-    val WaveOut = Output(UInt(18.W))
+    val WaveIn = Input(SInt(18.W))
+    val WaveOut = Output(SInt(18.W))
     val SampleType = Input(Bool()) // [input=0/output=1]
     val Enable = Input(Bool())
     val Completed = Output(Bool())
+
+    val CoeffAdress = Input(UInt(10.W))//TEMP
+    val Coeffdata = Input(SInt(18.W))//TEMP
+    val CoeffLoadEN = Input(Bool())//TEMP
   })
 
 
   //Wire definitions
-  val DSPOut = Wire(UInt())
-  val DSPIn = Wire(UInt())
-  val RegOutEN = Wire(UInt())
-  val IncrementCount = Wire(UInt())
-  val End = Wire(UInt())
 
-  val FIROutput = Wire(UInt())
-  val FIRInput = Wire(UInt())
+  val FIRInput = Wire(SInt())
   val CoeffCount = Wire(UInt())
   val SampleAdress = Wire(UInt())
   // Memory definitions
 
-  val InputSampleMemory = SyncReadMem(2048, UInt(18.W))
-  val OutputSampleMemory = SyncReadMem(2048, UInt(18.W))
-  val CoeffMemory = SyncReadMem(1024, UInt(18.W))
+  val InputSampleMemory = SyncReadMem(2048, SInt(18.W))
+  val OutputSampleMemory = SyncReadMem(2048, SInt(18.W))
+  val CoeffMemory = SyncReadMem(1024, SInt(18.W))
 
-  val OutputReg = Reg(UInt(18.W))
-  val MAccReg = Reg(UInt(18.W))
+  val OutputReg = Reg(SInt(18.W))
+  val MAccReg = Reg(SInt(36.W))
 
   val CountMax = (filterLength-1).U
   val SampleCount = Reg(UInt(11.W))
@@ -44,16 +42,19 @@ class IOFilter(filterLength: Int) extends Module {
   SampleCount:=0.U
   CoeffCount := 0.U
 
-  //MemControl FSM
-  //TODO Make control logic controlling the control bits
+  //TEMP COEFF MEMORY CONTROL
+  when(io.CoeffLoadEN){
+    CoeffMemory.write(io.CoeffAdress,io.Coeffdata)
+  }
+
 
   //Counter
 
   //output state:
   when(SampleCount === CountMax){
-    OutputReg:=FIROutput
+    OutputReg:=MAccReg << 18.U
     SampleCount:=0.U
-    MAccReg:=0.U
+    MAccReg:=0.S
 
     //CountUp state:
   }.elsewhen(SampleCount > 0.U | io.Enable){
@@ -61,21 +62,21 @@ class IOFilter(filterLength: Int) extends Module {
     CoeffCount := SampleCount
 
     //CountDown state:
-  }.elsewhen(SampleCount > CountMax(2,11)){
+  }.elsewhen(SampleCount > CountMax < 1.U){
     SampleCount:=SampleCount+1.U
     CoeffCount := CountMax-SampleCount
 
     //Ready state:
   }.otherwise{
     io.Completed := 1.U
-    MAccReg:=0.U
+    MAccReg:=0.S
   }
+
 
 
   //Input/output mode
   //Output mode
   when(io.SampleType){
-    FIRInput:=OutputSampleMemory(SampleAdress)
 
     when(OutputSamplePointer+SampleCount<CountMax) {
       SampleAdress := OutputSamplePointer + SampleCount
@@ -84,17 +85,33 @@ class IOFilter(filterLength: Int) extends Module {
     }
 
     //sample pointer
-    when(OutputSamplePointer<=CountMax & SampleCount===0.U){
+    when(OutputSamplePointer<=CountMax & SampleCount===0.U & io.Enable){
       OutputSamplePointer:=OutputSamplePointer + 1.U
-    }.elsewhen(SampleCount===0.U){
+    }.elsewhen(SampleCount===0.U & io.Enable){
       OutputSamplePointer:=0.U
     }
 
+    //sample handling
+    //todo check this out
+    when(SampleCount===1.U){
+      OutputSampleMemory.write(OutputSamplePointer-1.U,io.WaveIn)
+    }
+
+    when(SampleCount===0.U){
+      FIRInput:=io.WaveIn
+    }.otherwise{
+      FIRInput:=OutputSampleMemory.read(SampleAdress)
+    }
 
 
   //Input mode
   }.otherwise{
-    FIRInput:=InputSampleMemory(SampleAdress)
+
+    when(SampleCount===0.U){
+      FIRInput:=io.WaveIn
+    }.otherwise{
+      FIRInput:=InputSampleMemory.read(SampleAdress)
+    }
 
     when(InputSamplePointer+SampleCount<CountMax) {
       SampleAdress := InputSamplePointer + SampleCount
@@ -107,15 +124,18 @@ class IOFilter(filterLength: Int) extends Module {
       InputSamplePointer:=InputSamplePointer + 1.U
     }.elsewhen(SampleCount===0.U){
       InputSamplePointer:=0.U
-      SampleAdress
     }
-
+    //sample handling
+    when(io.Enable & SampleCount===0.U){
+      OutputSampleMemory.write(OutputSamplePointer-1.U,io.WaveIn)
+    }
   }
 
 
-
   //FIR filtering
-  MAccReg:=MAccReg + CoeffMemory(CoeffCount)*FIRInput     //note first filterlength of samples are garbage, but since they are gone in a split second it is fine
+  //todo make sure that the timing of  coeffMemory is ok
+  //todo below something is UInt instead of Int (:
+  MAccReg:=MAccReg + CoeffMemory.read(CoeffCount)*FIRInput     //note first filterlength of samples are garbage, but since they are gone in a split second it is fine
 
 
 
