f56c48c3863499d6663a77eaeee6c05649d57676
Karl Herman Krause <66793169+McHerman@users.noreply.github.com>
2022-06-15 13:33:54 +0200

    Delete Vivado_project directory

--- Vivado_project/project_with_chisel_file.ip_user_files/sim_scripts/xadc_wiz_0/xcelium/run.f
+++
@@ -1,7 +0,0 @@
--makelib xcelium_lib/xil_defaultlib \
-  "../../../../project_with_chisel_file.gen/sources_1/ip/xadc_wiz_0/xadc_wiz_0.v" \
--endlib
--makelib xcelium_lib/xil_defaultlib \
-  glbl.v
--endlib
-
