45824217971014b09c2f53c8b97b5481c254f6f2
Karl <karl@krauses.dk>
2022-06-17 21:46:08 +0200

    changed stuff

--- src/main/scala/I2S.scala
+++ src/main/scala/I2S.scala
@@ -1,51 +1,29 @@
-
 /*
-class MemoryController(Count: Int) extends Module {
+
+class I2S(Bitwidth: Int) extends Module {
   val io = IO(new Bundle {
     val Enable = Input(Bool())
-    //val WriteEnable = Input(Bool())
-    //val Address = Input(UInt(24.W))
-    //val WriteData = Input(UInt(18.W))
-
     val ReadData = Output(UInt(18.W))
-
-    //val Ready = Output(Bool())
     val Completed = Output(Bool())
   })
 
-  val SPI = IO(new Bundle{
+  val Out = IO(new Bundle{
     val SCLK = Output(Bool())
-    val MCLK = Output(Bool())
+    val LCLK = Output(Bool())
+    //val MCLK = Output(Bool())
     val SDOUT = Input(Bool())
-    val LCLK = Input(Bool())
   })
 
   // Register and state machine definitions.
 
-  val DataReg = RegInit(0.U(16.W))
-
-  SPI.CE := true.B
-  io.Completed := false.B
-  io.Ready := false.B
+  val DataRigthReg = RegInit(0.U(Bitwidth.W))
+  val DataLeftReg = RegInit(0.U(Bitwidth.W))
 
   io.ReadData := DataReg
 
-  SPI.SI := 0.U(4.W).asBools
-
-  SPI.Drive := false.B
-
-  //val boot :: resetEnable :: resetWait :: setReset :: idle :: read :: write :: quadWrite :: quadRead :: quadMode :: Nil = Enum(10)
-  val StateReg = RegInit(boot)
-
-  val transmitCMD :: transmitAddress :: transmitData :: writeDelay :: receiveData :: computeAddress :: Nil = Enum(6)
-  val SubStateReg = RegInit(transmitCMD)
-
   val CntReg = RegInit(0.U(14.W))
 
-  //val WriteDataReg = RegInit(0.U(16.W))
-  //val AddressReg = RegInit(0.U(24.W))
-
-  //val SPI_mode = RegInit(0.U(1.W))
+  val StateReg = RegInit(0.U(4.W))
 
   // Clock stuff
 
@@ -104,209 +82,32 @@ class MemoryController(Count: Int) extends Module {
 
   // Actual state machine
 
-
-  switch(StateReg) {
-    is(boot){
-      // Resets clock for reset command
-
-      CntReg := CntReg + 1.U
-
-      when(CntReg === 1.U){ //Set to 1 for testing, normally "h3fff"
-        SPI.CE := false.B
-        ClockReset := true.B
-        StateReg := resetEnable
-        CntReg := 0.U
-      }
-    }
-    is(resetEnable) {
-      SPI.CE := false.B
-      ClockEn := true.B
-      SPI.Drive := true.B
-
-      SPI.SI(1) := CMDResetEnable(7.U - CntReg)
-
-      when(NextStateInv){
+  switch(StateReg){
+    is(0.U){
+      when(RisingEdge){
+        DataRightReg := Cat(DataRightReg, SDOUT.asUInt)
         CntReg := CntReg + 1.U
       }
 
-      when(CntReg === 7.U && NextStateInv) {
-        SPI.CE := true.B
+      when(CntReg === Bitwidth.U){
         CntReg := 0.U
-        SPI.SI(1) := 0.U
-        StateReg := resetWait
+        StateReg := StateReg + 1.U
       }
     }
-    is(resetWait){
-      // A Delay between the two commands
-      SPI.CE := true.B
-
-      when(NextStateInv){
-        ClockReset := true.B
-        StateReg := setReset
-      }
-    }
-    is(setReset) {
-      SPI.CE := false.B
-      ClockEn := true.B
-      SPI.Drive := true.B
-
-      SPI.SI(1) := CMDReset(7.U - CntReg)
-
-      when(NextStateInv){
+    is(1.U){
+      when(RisingEdge){
+        DataLeftReg := Cat(DataRightReg, SDOUT.asUInt)
         CntReg := CntReg + 1.U
       }
 
-      when(CntReg === 7.U && NextStateInv) {
-        SPI.CE := true.B
+      when(CntReg === Bitwidth.U){
         CntReg := 0.U
-
-        StateReg := idle
-      }
-    }
-    is(idle) {
-      // Waits for command from main
-
-      SPI.CE := true.B
-      io.Ready := true.B
-
-      when(io.ReadEnable) {
-        switch(SPI_mode){
-          is(0.U){
-            StateReg := read
-          }
-          is(1.U){
-            StateReg := quadRead
-          }
-        }
-        SubStateReg := transmitCMD
-        SPI.CE := false.B
-        ClockReset := true.B
-        AddressReg := io.Address
-      }.elsewhen(io.WriteEnable) {
-        switch(SPI_mode){
-          is(0.U){
-            StateReg := write
-          }
-          is(1.U){
-            StateReg := quadWrite
-          }
-        }
-        SubStateReg := transmitCMD
-        SPI.CE := false.B
-        ClockReset := true.B
-        AddressReg := io.Address
-        WriteDataReg := io.WriteData
-      }
-    }
-    is(read) {
-      switch(SubStateReg) {
-        is(transmitCMD) {
-          SPI.CE := false.B
-          ClockEn := true.B
-          SPI.Drive := true.B
-
-          SPI.SI(1) := CMDSPIRead(7.U - CntReg)
-
-          when(NextStateInv){
-            CntReg := CntReg + 1.U
-          }
-
-          when(CntReg === 7.U && NextStateInv) {
-            CntReg := 0.U
-            SubStateReg := transmitAddress
-          }
-        }
-
-        is(transmitAddress) {
-          SPI.CE := false.B
-          ClockEn := true.B
-          SPI.Drive := true.B
-
-          SPI.SI(1) := AddressReg(23.U - CntReg)
-
-          when(NextStateInv){
-            CntReg := CntReg + 1.U
-          }
-
-          when(CntReg === 23.U && NextStateInv) {
-            CntReg := 0.U
-            SubStateReg := receiveData
-          }
-        }
-        is(receiveData) {
-          SPI.CE := false.B
-          ClockEn := true.B
-
-          // Reads on the rising edge of SCLK
-
-          when(RisingEdge){
-            DataReg := Cat(DataReg, SPI.SO(0).asUInt)
-            CntReg := CntReg + 1.U
-          }
-
-          when(CntReg === 15.U && NextStateInv) {
-            io.Completed := true.B
-            StateReg := idle
-          }
-        }
-      }
-    }
-    is(write) {
-      switch(SubStateReg) {
-        is(transmitCMD) {
-          SPI.CE := false.B
-          ClockEn := true.B
-          SPI.Drive := true.B
-
-          SPI.SI(1) := CMDSPIWrite(7.U - CntReg)
-
-          when(NextStateInv){
-            CntReg := CntReg + 1.U
-          }
-
-          when(CntReg === 7.U && NextStateInv) {
-            CntReg := 0.U
-            SubStateReg := transmitAddress
-          }
-        }
-        is(transmitAddress) {
-          SPI.CE := false.B
-          ClockEn := true.B
-          SPI.Drive := true.B
-
-          SPI.SI(1) := AddressReg(23.U - CntReg)
-
-          when(NextStateInv){
-            CntReg := CntReg + 1.U
-          }
-
-          when(CntReg === 23.U && NextStateInv){
-            CntReg := 0.U
-            SubStateReg := transmitData
-          }
-        }
-        is(transmitData) {
-          SPI.CE := false.B
-          ClockEn := true.B
-          SPI.Drive := true.B
-
-          SPI.SI(1) := WriteDataReg(15.U - CntReg)
-
-          when(NextStateInv){
-            CntReg := CntReg + 1.U
-          }
-
-          when(CntReg === 15.U && NextStateInv) {
-            CntReg := 0.U
-            io.Completed := true.B
-            StateReg := idle
-            SPI.CE := true.B
-          }
-        }
+        StateReg := StateReg + 1.U
       }
     }
   }
 }
 */
+
 
 
